83c29fdbd4f4a71d39873b47f836b9975ec29f4e
Dan Toft <tanddant@gmail.com>
2023-06-14 18:27:29 +0200

    🎉 - Init repo

---
+++ src/webparts/tenantWideExtensionManager/components/ITenantWideExtensionManagerProps.ts
@@ -0,0 +1,7 @@
+export interface ITenantWideExtensionManagerProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
